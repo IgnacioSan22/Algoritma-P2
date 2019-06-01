af5bee54db5c9859e2408f8014d69d9044f55e64
Fran Ferraz <franferrazg@gmail.com>
2019-06-01 13:38:48 +0200

    Comentar main

--- main.cc
+++ main.cc
@@ -10,16 +10,30 @@ using namespace std;
 
 #define paradas 7
 
+/*
+ * Estructura que representa un pedido mediante sua parada de inicio, de fin
+ * y el número de pasajeros que contiene.
+ */
 struct pedido {
     int inicio, fin, pasajeros;
 };
 
+/*
+ * Pre: p1 != null && p2 != null
+ * Post: Devuelve verdadero si el beneficio de p1 es mayor al de p2, falso
+ * en caso contrario.
+ */
 bool cmpBeneficio(pedido p1, pedido p2){
     int b1 = (p1.fin - p1.inicio) * p1.pasajeros;
     int b2 = (p2.fin - p2.inicio) * p2.pasajeros;
     return b1 > b2;
 }
 
+/*
+ * Pre: !ocupacion.isEmpty() && ocupacion.size() <= (p.fin - p.inicio)
+ * Post: Devuelve verdadero en caso de que añadir el pedido a la ocupación no
+ * exceda la capacidad del tren, falso en caso contrario.
+ */
 bool cabePedido(const vector<int>& ocupacion, pedido p, int tren)
 {
     for(int i = p.inicio; i <= p.fin ; i++){
@@ -30,14 +44,23 @@ bool cabePedido(const vector<int>& ocupacion, pedido p, int tren)
     return true;
 }
 
+/*
+ * Pre: ocupacion != null
+ * Post: añade el pedido p a la ocupación del tren
+ */
 void actOcupacion(vector<int>& ocupacion, pedido p)
 {
-    for (int i = p.inicio; i < p.fin; i++)//he quitado el = de i <= p.fin
+    for (int i = p.inicio; i < p.fin; i++)
     {
         ocupacion[i] = ocupacion[i] + p.pasajeros;
     }
 }
 
+/*
+ * Pre: lista != null && ocupacion != null
+ * Post: Devuelve el coste de añadir secuencialmente todos los pedidos de lista
+ * mientras no se exceda la capacidad del tren
+ */
 int lowerBound(vector<pedido> lista, vector<int> ocupacion, int ultimo, int beneficio, int tren)
 {
     int lb = beneficio;
@@ -52,6 +75,7 @@ int lowerBound(vector<pedido> lista, vector<int> ocupacion, int ultimo, int bene
     return lb;
 }
 
+// ESTE HACE FALTA ??
 void actOcupacion(vector<int>& ocupacion, int ini, int fin, int p)
 {
     for (int i = ini; i <= fin; i++)
@@ -60,6 +84,11 @@ void actOcupacion(vector<int>& ocupacion, int ini, int fin, int p)
     }
 }
 
+/*
+ * Pre: true
+ * Post: Devuelve el número de plazas libres hasta el momento en el punto de
+ * mayor ocupación entre las paradas ini y fin
+ */
 int faltan(const vector<int>& ocupacion, int fin, int ini, int tren)
 {
     int sum = 0, max = ocupacion[ini];
@@ -70,6 +99,11 @@ int faltan(const vector<int>& ocupacion, int fin, int ini, int tren)
     return tren - max;
 }
 
+/*
+ * Pre: lista != null && ocupacion != null
+ * Post: Devuelve el coste de añadir secuencialmente todos los pedidos de lista
+ * mientras no se exceda la capacidad del tren, permitiendo fraccionar pedidos
+ */
 int costeEstimado(vector<pedido> lista, vector<int> ocupacion, int ultimo, int beneficio, int tren)
 {
     int lb = beneficio;
@@ -90,6 +124,11 @@ int costeEstimado(vector<pedido> lista, vector<int> ocupacion, int ultimo, int b
     return lb;
 }
 
+/*
+ * Pre: vector != null
+ * Post: Devuelve verdadero en caso de que ninguna de las componentes de oc 
+ * (ocupacion) exceda la capacidad del tren.
+ */
 bool factible(const vector<int>& oc, int tren){
     for (int i = 0; i < oc.size(); i++)
     {
@@ -101,18 +140,27 @@ bool factible(const vector<int>& oc, int tren){
     return true;
 }
 
+/*
+ * Pre: entrada.is_open() && pedidos <= entrada.numLines()
+ * Post: Dada una lista de pedidos de una estación individual para un tren de
+ * capacidad = tren, número de estaciones = estaciones y número de pedidos
+ * = pedidos, calcula mediante un algoritmo de ramificación y poda el máximo
+ * ingreso obtenible que no exceda las capacidades de la línea.
+ */
 void resolver(int tren, int estaciones, int pedidos, ifstream& entrada) {
-     cout << "Datos problema: " << tren << ", " << estaciones << ", " << pedidos << endl; 
-        //struct pedido lista[pedidos];
-        vector<pedido> lista;
+    cout << "Datos problema: " << tren << ", " << estaciones << ", " << pedidos << endl; 
 
-        int begin, fin, pasj, elem = 0;
-        struct pedido p_auxiliar;
+    /* Declaración de variables */
+    vector<pedido> lista;
+    int begin, fin, pasj, elem = 0;
+    struct pedido p_auxiliar;
 
+    /* Si una lista incluye más de 0 pedidos, se hace el cálculo */
     if(pedidos > 0){
+
+        /* Creación de la lista de pedidos */
         for(int i = 0; i < pedidos; i++){
             entrada >> begin >> fin >> pasj;
-            //entrada >> lista[i].inicio >> lista[i].fin >> lista[i].pasajeros;
             if(pasj <= tren){
                 p_auxiliar.inicio = begin; p_auxiliar.fin = fin; p_auxiliar.pasajeros = pasj;
                 lista.push_back(p_auxiliar);
@@ -120,12 +168,14 @@ void resolver(int tren, int estaciones, int pedidos, ifstream& entrada) {
         }
         pedidos = lista.size();
 
-        //Inicio real de la resolucion del programa
+        /* Inicio real de la resolucion del programa */
         clock_t start, end;
         start = clock();
 
+        /* Se ordena la lista por beneficio */
         sort(lista.begin(), lista.end(), cmpBeneficio);
         
+        /* Creación de las estructuras para resolver */
         int actualLB = 0, mejor_sol = 0;
         Heap nodos_vivos;
 
@@ -140,7 +190,10 @@ void resolver(int tren, int estaciones, int pedidos, ifstream& entrada) {
         cout << "Primer coste estimado: " << coste << endl;
         cout << "Primer LB: " << lower_bound << endl;
 
+        /* Si se puede aceptar algún pedido, se hace el cálculo */
         if(lower_bound > 0){
+
+            /* Creación del árbol como cola de prioridades y parametrización */
             actualLB = lower_bound;
             raiz.coste_estimado = coste;
             raiz.lb = lower_bound;
@@ -151,31 +204,24 @@ void resolver(int tren, int estaciones, int pedidos, ifstream& entrada) {
             Nodo aux, izq, der;
             struct pedido paux;
 
-            while(!nodos_vivos.esVacia() /*&& iter < 6*/)
+            /* La búsqueda termina cuando se han podado todas las opciones */
+            while(!nodos_vivos.esVacia())
             {
                 iter++;
-                //cout << "En el bucle\n";
                 aux = nodos_vivos.cima(); nodos_vivos.pop();
 
                 i = aux.order;
 
-                //creacion hijo izquierdo, es decir, se acepta siguiente peticion
-                //cout << "-------Hijo izq----------\n";
+                /* Creación hijo izquierdo, es decir, se acepta siguiente petición */
                 paux = lista[i];
                 ocupacion_aux = aux.ocupacion;
                 actOcupacion(ocupacion_aux,paux);
-                /*for(int i = 0; i < 7; i++)
-                {
-                    cout << "{" << ocupacion_aux[i] << "}";
-                }
-                cout << endl;*/
+
+                /* Si es factible, se añade a la solución y se actualizan los parámetros */
                 if(factible(ocupacion_aux,tren)){
-                    //cout << "Nodo factible\n";
                     ben_aux = aux.beneficio_actual + ((paux.fin - paux.inicio) * paux.pasajeros);
                     coste = costeEstimado(lista,ocupacion_aux,i+1,ben_aux,tren);
                     lower_bound = lowerBound(lista,ocupacion_aux,i+1,ben_aux,tren);
-                    //cout << "Hijo izq coste estimado: " << coste << endl;
-                    //cout << "Hijo izq LB: " << lower_bound << endl;
                     if (coste >= actualLB){
                         if(i < (pedidos-1)){
                             izq = Nodo(coste, lower_bound, i+1, ocupacion_aux, ben_aux);
@@ -184,28 +230,17 @@ void resolver(int tren, int estaciones, int pedidos, ifstream& entrada) {
                         if (lower_bound > actualLB){
                             actualLB = lower_bound;
                         }
-                        if (i == (pedidos-1) && mejor_sol < lower_bound)
-                        //if (coste == lower_bound)
-                        {
+                        if (i == (pedidos-1) && mejor_sol < lower_bound){
                             mejor_sol = coste;
                         }
                     }
                 }
-                //cout << "---------------------\n";
 
-                //creacion hijo derecho, es decir, se rechaza siguiente peticion
-                //cout << "-------Hijo der----------\n";
+                /* Creacion hijo derecho, es decir, se rechaza siguiente peticion */
                 ocupacion_aux = aux.ocupacion;
-                /*for (int i = 0; i < 7; i++)
-                {
-                    cout << "{" << ocupacion_aux[i] << "}";
-                }
-                cout << endl;*/
                 ben_aux = aux.beneficio_actual;
                 coste = costeEstimado(lista, ocupacion_aux, i+1, ben_aux, tren);
                 lower_bound = lowerBound(lista, ocupacion_aux, i+1, ben_aux, tren);
-                //cout << "Hijo der coste estimado: " << coste << endl;
-                //cout << "Hijo der LB: " << lower_bound << endl;
                 if (coste >= actualLB)
                 {
                     if (i < (pedidos - 1))
@@ -217,25 +252,26 @@ void resolver(int tren, int estaciones, int pedidos, ifstream& entrada) {
                     {
                         actualLB = lower_bound;
                     }
-                    if (i == (pedidos - 1) && mejor_sol < lower_bound)
-                    //if (coste == lower_bound)
-                    {
+                    if (i == (pedidos - 1) && mejor_sol < lower_bound){
                         mejor_sol = coste;
                     }
                 }
-                //cout << "---------------------\n";
             }
             end = clock();
 
+            /* Se muestra la solcución por la salida estándar */
             double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
             cout << "Time taken by program is : " << fixed
                 << time_taken << setprecision(10);
             cout << " sec " << endl;
 
             cout << "La mejor solucion encontrada ha sido: " << mejor_sol << endl;
-        } else {
+        } 
+        /* Si NO se puede aceptar ningúngún pedido, no se hace el cálculo */
+        else {
             end = clock();
             
+            /* Se muestra la solcución por la salida estándar */
             double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
             cout << "Time taken by program is : " << fixed
                 << time_taken << setprecision(10);
@@ -245,10 +281,14 @@ void resolver(int tren, int estaciones, int pedidos, ifstream& entrada) {
         }
 
         
-    } else {
+    } 
+    /* Si una lista NO incluye más de 0 pedidos, no se hace el cálculo */
+    else {
         clock_t start, end;
         start = clock();
         end = clock();
+
+        /* Se muestra la solcución por la salida estándar */
         double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
         cout << "Time taken by program is : " << fixed
              << time_taken << setprecision(10);
@@ -260,6 +300,11 @@ void resolver(int tren, int estaciones, int pedidos, ifstream& entrada) {
 
 }
 
+/*
+ * Pre: argc >=2, argv[1] es un fichero que incluye los datos del problema
+ * Post: Escribe en el fichero argv[2] la solución óptima de selección de 
+ * pedidos para cada una de las instancias de problema.
+ */
 int main(int argc, char *argv[]){
     
     if(argc >= 2){
@@ -276,7 +321,7 @@ int main(int argc, char *argv[]){
             return -1;
         }
         
-        cout.rdbuf(salida.rdbuf()); //redirect cout to salida
+        cout.rdbuf(salida.rdbuf()); //Redirigir stdout al fichero de salida
 
         int tren, estaciones, pedidos;
         entrada >> tren >> estaciones >> pedidos;
